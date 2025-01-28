8d264b54365e47b0c08407e79f389c0ef101ff96
Luis Pereira <luispereira.tkd@gmail.com>
2025-01-28 12:38:17 +0000

    parser: start document block parsing by type

--- main.scm
+++ main.scm
@@ -2,6 +2,7 @@
   (json)
   (ice-9 match))
 
+; (define confluence-page-export "doc-full.json")
 (define confluence-page-export "doc-simple.json")
 
 (define (load-json-file path)
@@ -19,5 +20,30 @@
          (content (assoc-ref atlas "value")))
     (json-string->scm content)))
 
-(format #t "~a\n" (assoc-ref
-                    (get-page-atlas confluence-page-json) "type"))
+; (display confluence-page-json)
+
+; (format #t "~a\n" (assoc "body" confluence-page-json))
+; (display (assoc-ref confluence-page-json "spaceId"))
+; (display (car confluence-page-json))
+
+(define (parse-text-block block)
+  (assoc-ref block "text"))
+
+(define (parse-bodied-extension-block block)
+  (string-append 
+    (assoc-ref block "title")
+    (map atlas->md (assoc-ref block "content"))))
+
+(define (default-parse-block block)
+    (map atlas->md (assoc-ref block "content")))
+
+(define (atlas->md block)
+  (let ((type (assoc-ref block "type")))
+    (format #t "Evaluating type ~a\n" type)
+    (match type
+           ("doc" (default-parse-block block))
+           (_ (format #f "Unknown '~a' type\n" type)))))
+
+(display (atlas->md (get-page-atlas confluence-page-json)))
+; (format #t "~a\n" (assoc-ref
+;                     (get-page-atlas confluence-page-json) "type"))
