0a44afe2dadcf28698d282b695622a0ef1df5885
Luis Pereira <luispereira.tkd@gmail.com>
2025-02-21 22:46:38 +0000

    cli: add options for config and API variables

--- src/config/cli.scm
+++ src/config/cli.scm
@@ -1,16 +1,32 @@
 (define-module (config cli)
+    #:use-module (json)
     #:use-module (ice-9 getopt-long)
     #:export (load-options
+               load-config
+            valid-cli-argument
                save-markdown))
 
 
 (define option-spec
   '((output (single-char #\o) (value #t))
+    (logger (single-char #\l) (value #f))
     (log-file (value #t))
-    (logger (single-char #\l) (value #f))))
+    (config (single-char #\c) (value #t))
+    (token (value #t))
+    (base-url (single-char #\b) (value #t))
+    (page-id (single-char #\p) (value #t))))
 
 (define (load-options)
   (getopt-long (command-line) option-spec))
+
+(define (load-config file-path)
+  (if (access? file-path R_OK)
+    (with-input-from-file
+      file-path
+      (lambda ()
+        (json->scm)))
+    #f))
+
 
 (define (save-markdown markdown output-file)
   (if (and output-file (not (string-null? output-file)))
@@ -19,3 +35,6 @@
       (lambda ()
         (format #t "~a\n" markdown)))
     (format #t "~a\n" markdown)))
+
+(define (valid-cli-argument arg)
+  (and (string? arg) (not (string-null? arg))))
